ef50ade20d400120f46c28bc84463433ddb7f60e
hector20066 <hectorluisacevedocontreras00@gmail.com>
2026-09-01 21:25:04 -0500

    feat: crear estructura basica del encabezado

---
+++ view/public/menu.php
@@ -0,0 +1,37 @@
+<?php
+    print("Hola mundo")     
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Menú - Grano & Alma</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@500;600;700&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <link rel="icon" href="img/favicon.svg">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div>
+                <a href="../../index.html"><img src="../../img/favicon.svg" alt="" width="30" height="30">Grano & Alma</a>
+
+                <nav>
+                    <a href="../../index.html">Inicio</a>
+                    <a href="nosotros.html">Nosotros</a>
+                    <a href="menu.php">Menú</a>
+                    <a href="contacto.html">Contacto</a>
+                </nav>
+            </div>
+
+            <div>
+                <a href="menu.php">Ver menú</a>
+
+                <button>
+                    <span></span><span></span><span></span>
+                </button>
+            </div>
+        </header>
+    </body>
+</html>
