b573a8c1b2513cc22b0d97bcfa0e55375807b20a
hector20066 <hectorluisacevedocontreras00@gmail.com>
2026-09-04 09:51:32 -0500

    feat: agregar archivos necesarios para la capa DAO

---
+++ model/dto/DTOProductsDetails.php
@@ -0,0 +1,16 @@
+<?php
+
+use Decimal\Decimal;
+
+class DTOProductsDetails {
+
+        private int $id;
+        private string $category;
+        private string $name;
+        private string $description;
+        private Decimal $price;
+        private string $image;
+
+    }
+
+?>
