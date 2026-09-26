8c66ce896f305ea0e422d0874bda7696d798e51d
hector20066 <hectorluisacevedocontreras00@gmail.com>
2026-09-26 13:39:27 -0500

    feat: agregar dto para la informacion al iniciar sesion

---
+++ model/dto/DTOUsersLogin.php
@@ -0,0 +1,55 @@
+<?php
+
+class DTOUsersLogin {
+
+    private int $id;
+    private string $name;
+    private string $email;
+    private Roles $role;
+    private string $passwd;
+
+    public function __construct() {
+
+    }
+
+    public function getId() : int {
+        return $this->id;
+    }
+
+    public function setId(int $id) : void {
+        $this->id = $id;
+    }
+
+    public function getName() : string {
+        return $this->name;
+    }
+
+    public function setName(string $name) : void {
+        $this->name = $name;
+    }
+
+    public function getEmail() : string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email) : void {
+        $this->email = $email;
+    }
+
+    public function getRole() : Roles {
+        return $this->role;
+    }
+
+    public function setRole(Roles $role) : void {
+        $this->role = $role;
+    }
+
+    public function getPasswd() : string {
+        return $this->passwd;
+    }
+
+    public function setPasswd(string $passwd) : void {
+        $this->passwd = $passwd;
+    }
+
+}
