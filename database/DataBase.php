e108880f13d97e2997a26a62c8bcaaa5beb865a7
hector20066 <hectorluisacevedocontreras00@gmail.com>
2026-09-26 14:36:54 -0500

    feat: agregar archivo para conectar a la base de datos

---
+++ database/DataBase.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../config/env.php';
+
+class DataBase {
+    protected PDO $connect;
+
+    public function __construct() {
+        try {
+            $dsn = sprintf('%s:host=%s;dbname=%s;port=%s;charset=utf8mb4', $_ENV['DB_DRIVER'], $_ENV['DB_LOCAL_HOST'], $_ENV['DB_LOCAL_NAME'], $_ENV['DB_PORT']);
+
+            $this->connect = new PDO($dsn, $_ENV['DB_LOCAL_USER'], $_ENV['DB_LOCAL_PASSWD'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                ]
+            );
+
+        } catch (PDOException $e) {
+            throw new RuntimeException('Error al conectar a la base de datos.');
+        }
+    }
+
+}
