030b6899e26fce3e14790543db8221cfd18e8255
hector20066 <hectorluisacevedocontreras00@gmail.com>
2026-09-26 09:13:06 -0500

    refactor: agregar nueva columna a la tabla de usuarios

--- DATABASE.sql
+++ DATABASE.sql
@@ -28,5 +28,6 @@ CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     passwd VARCHAR(300) NOT NULL,
-    role VARCHAR(40) NOT NULL
+    role VARCHAR(40) NOT NULL,
+    status VARCHAR(40) NOT NULL
 );
